f5400c2880d5543557af0b498bb23dfcf0a84a17
Yusuf Khan <ykhan5@cougarnet.uh.edu>
2024-10-01 20:44:21 -0500

    Update p1.py

--- Unit3/Session1/p1.py
+++ Unit3/Session1/p1.py
@@ -1,8 +1,22 @@
-
 class Solution:
     
     def mostWater(self, height):
-        print("Hello Guys!")
+        l, r = 0, len(height) - 1
+
+        maxArea = 0
+
+        while l < r:
+            area = min(height[l], height[r]) * (r-l)
+
+            maxArea = max(area, maxArea)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxArea
+            
     
 
 height = [1,8,6,2,5,4,8,3,7]
