6e777889264bb63ed2a1d3cfa7472e03d0d07ff0
Yusuf Khan <ykhan5@cougarnet.uh.edu>
2024-11-12 20:50:32 -0600

    Unit 9 Session 1 P1

--- Unit9/Session1/p1.py
+++ Unit9/Session1/p1.py
@@ -1,9 +1,38 @@
 class Solution:
-    def accountsMerge(accounts):
-        pass
+    def longestPalindrome(s):
+        if not s:
+            return ""
+        
+        start = 0
+        end = 0
+
+        def expandPalindrome(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+
+            return right - left - 1
+
+        for i in range(len(s)):
+            both_sides = expandPalindrome(s, i, i)
+            one_side = expandPalindrome(s, i, i + 1)
+            max_len = max(both_sides, one_side)
+
+            if max_len > (end - start + 1):
+
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start:end+1]
+            
+        
+        
             
     
 
-accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+s = "babad"
 solution = Solution()
-print(solution.find(accounts))
+print(solution.longestPalindrome(s))
+s = "bbc"
+solution = Solution()
+print(solution.longestPalindrome(s))
